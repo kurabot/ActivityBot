d0d99ab7ff87e925e63739e7767041125d8ebb6d
Arkteus <44676873+Arkteus@users.noreply.github.com>
2019-02-05 20:31:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,6 @@ bot.on('message', msg => {
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
 
 });
 
