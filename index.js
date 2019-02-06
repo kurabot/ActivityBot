9e5bda80a46179eec66ebf26f940d1d86fdefa00
kurasoooooooooooooo <35342616+kurabot@users.noreply.github.com>
2019-02-06 14:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = ("?");
 
-bot.on('ready', function () {
+bot.on('ready', function () { 
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
 });
